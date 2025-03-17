5adaf4c81707ef4d0e8ea3e83a5eaa9824b1f5a3
grayshall <64482280+grayshall@users.noreply.github.com>
2025-03-17 20:04:11 +0800

    Update and rename d1.sh.txt to d1.sh

--- d1.sh
+++ d1.sh
@@ -1,6 +1,6 @@
 # add a feed source
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-dark' feeds.conf.defaultmatter
+sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-dark' feeds.conf.default
 sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
 #sed -i '$a src-git packages https://github.com/DHDAXCW/packages' feeds.conf.default
